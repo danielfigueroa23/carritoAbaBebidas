8261ae89ec31e749be5b7b92ecd9fe4273ad0eeb
daniel.figueroa <daniel.figueroa@artekium.com>
2024-03-30 11:32:38 -0300

    first commit - Basic Estructure Project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import User from './User'
+
+function Header() {
+    return (
+        <>
+            <Navbar/>
+            <User/>
+        </>
+    )
+}
+
+export default Header
